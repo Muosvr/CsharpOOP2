41b88b403d3d80ac472921b952fb8850c605c12b
Muosvr <lujason2015@gmail.com>
2018-08-18 13:29:50 -0400

    update

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine("Kitchen","Brand","DM1000");
+            myMachine.Location = "Kitchen";
+            myMachine.Model = "DM1000";
+
+            Console.WriteLine(myMachine.Location);
+            Console.WriteLine(myMachine.Make);
+            Console.WriteLine(myMachine.Model);
+            myMachine.MakeCappuccino();
+        }
+    }
+}
